fd4847f1e724c7efcdf5e9fa02328ca366a5a144
Jakub-Wilczak <jakub.alek.wilczak@gmail.com>
2025-02-27 16:32:29 +0100

    Regex for items and hideout ready

---
+++ tarkov_hideout/server/server.js
@@ -0,0 +1,29 @@
+﻿const fs = require('fs').promises;
+
+async function readFileAndProcess() {
+    try {
+        return await fs.readFile("hideout_items_list", "utf8");
+    } catch (err) {
+        console.error("FILE NOT FOUND", err);
+    }
+}
+
+async function main()
+{
+
+    let file_data = await readFileAndProcess();
+    // console.log(file_data);
+
+
+    
+    const pattern = /([^﻿].+)\r\n/g;
+    let matches = [... file_data.matchAll(pattern)];
+    
+    matches.forEach(match => {
+        console.log(match[1]);
+    })
+    
+}
+
+main();
+
